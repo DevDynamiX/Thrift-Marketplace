c88dbe1cd4286481ad4038de7794d81350b80423
tyrique <tyriquedebruin@gmail.com>
2024-09-23 23:32:00 +0200

    removed the incorrect TailWind CSS and rather installed NativeWind

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import "./tailwind.css";
 
-export default function Page() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+      <SafeAreaView style={styles.container}>
+          <View style={styles.container}>
+              <Text style={styles.title}>Welcome to Thrift Market!</Text>
+              <Text>you can start by editing Index.tsx</Text>
+          </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
