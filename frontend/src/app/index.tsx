83597cba7cb7054b0dec84ec862331d054bc99ca
tyrique <tyriquedebruin@gmail.com>
2024-10-10 15:59:14 +0200

    Installed and setup Expo Router along with the dependencies.

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import {router} from "expo-router";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
-export default function Page() {
+const IndexPage = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <Text style={styles.title}>Howzit MaseKint</Text>
+          <Text style={styles.title}>Welcome to Thrift Market!</Text>
+          <Text style={styles.subtitle}>This page is till a working in progress</Text>
+          <Text style={styles.subtitle}>Currently testing how the Expo Router works via Pressable</Text>
+          <Pressable
+              onPress={() =>
+                  router.push({
+                    pathname: "/tempFolderForRouting/[Id]",
+                    params: {Id: 1},
+                  })}
+          >
+            <Text>Click here to go "into" the app :)</Text>
+          </Pressable>
+        </View>
       </View>
-    </View>
   );
-}
+};
+
+export default IndexPage;
 
 const styles = StyleSheet.create({
   container: {
