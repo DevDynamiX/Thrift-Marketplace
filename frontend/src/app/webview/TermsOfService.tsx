e97e5e016d4c3f661ea33d2bbdb85e2db74c644c
ikagengthekiso <ikagengthekiso@gmail.com>
2024-11-09 10:56:43 +0200

    webview loader

--- frontend/src/app/webview/TermsOfService.tsx
+++ frontend/src/app/webview/TermsOfService.tsx
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import { WebView } from 'react-native-webview';
 import Constants from 'expo-constants';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+    const [loading, setLoading] = useState(true); // Track loading state
+
     return (
-        <WebView
-            style={styles.container}
-            source={{ uri: 'https://thriftmarket.netlify.app' }}
-        />
+        <View style={styles.container}>
+
+            {loading && (
+                <View style={styles.loader}>
+                    <ActivityIndicator size="large" color="#219281FF" />
+                </View>
+            )}
+
+
+            <WebView
+                style={styles.webview}
+                source={{ uri: 'https://thriftmarket.netlify.app' }}
+                onLoadStart={() => setLoading(true)}
+                onLoadEnd={() => setLoading(false)}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+            />
+        </View>
     );
 }
 
@@ -15,5 +32,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: Constants.statusBarHeight,
+    },
+    webview: {
+        flex: 1,
+    },
+    loader: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: [{ translateX: -25 }, { translateY: -25 }],
+        zIndex: 1,
     },
 });
